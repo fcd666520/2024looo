c143382dec970d05593cc904a75081732424d01e
yskk <xiaowei233333@gmail.com>
2023-04-21 13:13:58 +0000

    更新 '666/js/AGE动漫.js'

--- 666/js/AGE动漫.js
+++ 666/js/AGE动漫.js
@@ -1,7 +1,7 @@
 var rule = {
 	    title:'AGE动漫',
 	    // host:'https://www.agemys.cc',
-	    host:'https://www.agemys.net',
+	    host:'http://www.agemys.vip',
 	    url:'/catalog/all-all-all-fyclass-all-fyfilter',
 	    searchUrl:'/search?query=**&page=fypage',
 	    searchable:2,
