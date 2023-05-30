516fde60e5387165f157d4281b12eacb76d6c19b
fcd666520 <xiaowei233333@gmail.com>
2023-05-30 03:57:53 +0000

    Update NY影院.js

--- 666/js/NY影院.js
+++ 666/js/NY影院.js
@@ -2,7 +2,7 @@ muban.短视2.二级.title = '.slide-info-title&&Text;.hl-ma0&&Text';
 muban.短视2.二级.img = '.detail-pic&&img&&data-original';
 muban.短视2.二级.desc = '.fraction&&Text;;;.slide-info:eq(3)--strong&&Text;.slide-info:eq(2)--strong&&Text';
 var rule={
-    title:'NY影院',
+    title:'NY',
     模板:'短视2',
     host:'https://www.nycvod.com',
     homeUrl:'/index.php/label/rb.html',
