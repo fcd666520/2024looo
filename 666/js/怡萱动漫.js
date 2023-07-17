24dccc09d5c95328be47dc454186926c8806ba12
fcd666520 <xiaowei233333@gmail.com>
2023-07-17 23:24:19 +0000

    Update 怡萱动漫.js

--- 666/js/怡萱动漫.js
+++ 666/js/怡萱动漫.js
@@ -1,6 +1,6 @@
 var rule={
 	title:'怡萱动漫',
-	host:'https://www.yxdmlove.com',
+	host:'https://www.yxdmgo.com',
 	// url:'/category.html?channel=17&zhonglei=fyclass&orderby=pubdate&totalresult=2999&pageno=fypage',
 	url:'/category.html?channel=17&zhonglei=fyclassfyfilter&pageno=fypage',
 	filterable:1,//是否启用分类筛选,
